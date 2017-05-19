bfc7c2a971a664fa4b8fb47f3b06bd6a7b7876c2
liyuequn <18511909124@163.com>
2017-05-19 13:17:12 +0800

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,17 +17,6 @@ class DatabaseSeeder extends Seeder
         Model::reguard();
 
         // $this->call(UsersTableSeeder::class);
-        for ($i=0;$i<10;$i++){
-            DB::table('users')->insert([
-                'name' => str_random(10),
-                'email' => str_random(10).'@gmail.com',
-                'password' => bcrypt('secret'),
-                'phone' => rand(18511909100,18511909124),
-
-            ]);
-        }
-       
-
 
     }
 }
