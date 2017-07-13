8bf1709d567fbb023da468246a15ab7e9116849d
wangqian188 <m18811383660_1@163.com>
2017-07-13 14:34:17 +0800

    11

--- routes/web.php
+++ routes/web.php
@@ -81,4 +81,4 @@ Route::post('user/batchRemoveUser','UserController@batchRemoveUser');
 Route::get('office/loupanList','Contract\officeController@loupanList');
 Route::get('office/loudongList','Contract\officeController@loudongList');
 Route::get('office/fanghaoList','Contract\officeController@fanghaoList');
-
+//hello world
