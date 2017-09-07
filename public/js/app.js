f03201f5043279d34d6bf972ca771b11f5b029da
zhoumeng2012bj <zhoumeng2012bj@163.com>
2017-09-07 19:16:11 +0800

    button

--- public/js/app.js
+++ public/js/app.js
@@ -116871,37 +116871,37 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
           staticClass: "el-icon-caret-bottom el-icon--right"
         })]), _vm._v(" "), _c('el-dropdown-menu', {
           slot: "dropdown"
-        }, [(_vm.ztin(scope.row, [0, 1, 3, 4])) ? _c('el-dropdown-item', [_c('el-button', {
+        }, [(_vm.ztin(scope.row, [0, 1, 3, 4]) && _vm.fun('receivableAdd')) ? _c('el-dropdown-item', [_c('el-button', {
           on: {
             "click": function($event) {
               _vm.handleRokeBack(scope.$index, scope.row)
             }
           }
-        }, [_vm._v("提交收款")])], 1) : _vm._e(), _vm._v(" "), (scope.row.shiugaizhuangtai == '已修改') ? _c('el-dropdown-item', [_c('el-button', {
+        }, [_vm._v("提交收款")])], 1) : _vm._e(), _vm._v(" "), (scope.row.shiugaizhuangtai == '已修改' && _vm.fun('receivableAdd')) ? _c('el-dropdown-item', [_c('el-button', {
           on: {
             "click": function($event) {
               _vm.handleOpen(scope.$index, scope.row)
             }
           }
-        }, [_vm._v("修改记录")])], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [1, 2, 3, 4])) ? _c('el-dropdown-item', [_c('el-button', {
+        }, [_vm._v("修改记录")])], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [1, 2, 3, 4]) && _vm.fun('receivableAdd')) ? _c('el-dropdown-item', [_c('el-button', {
           on: {
             "click": function($event) {
               _vm.handleOpenUp(scope.$index, scope.row)
             }
           }
-        }, [_vm._v("提交记录")])], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [0, 1, 3, 4])) ? _c('el-dropdown-item', [(scope.row.sktype < 20) ? _c('el-button', {
+        }, [_vm._v("提交记录")])], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [0, 1, 3, 4]) && _vm.fun('receivableAdd')) ? _c('el-dropdown-item', [(scope.row.sktype < 20) ? _c('el-button', {
           on: {
             "click": function($event) {
               _vm.handleEdit(scope.$index, scope.row)
             }
           }
-        }, [_vm._v("编辑收款日期")]) : _vm._e()], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [0, 1, 3, 4])) ? _c('el-dropdown-item', [(scope.row.sktype < 20) ? _c('el-button', {
+        }, [_vm._v("编辑收款日期")]) : _vm._e()], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [0, 1, 3, 4]) && _vm.fun('receivableAdd')) ? _c('el-dropdown-item', [(scope.row.sktype < 20) ? _c('el-button', {
           on: {
             "click": function($event) {
               _vm.handleMoneyEdit(scope.$index, scope.row)
             }
           }
-        }, [_vm._v("编辑收款金额")]) : _vm._e()], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [0, 1, 3, 4])) ? _c('el-dropdown-item', [(scope.row.sktype == 20) ? _c('el-button', {
+        }, [_vm._v("编辑收款金额")]) : _vm._e()], 1) : _vm._e(), _vm._v(" "), (_vm.ztin(scope.row, [0, 1, 3, 4]) && _vm.fun('receivableAdd')) ? _c('el-dropdown-item', [(scope.row.sktype == 20) ? _c('el-button', {
           on: {
             "click": function($event) {
               _vm.handleEditYS(scope.$index, scope.row)
@@ -125535,13 +125535,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
               _vm.handleReject(scope.$index, scope.row)
             }
           }
-        }, [_vm._v("驳回付款")])], 1) : _vm._e(), _vm._v(" "), _c('el-dropdown-item', [_c('el-button', {
-          on: {
-            "click": function($event) {
-              _vm.handleOpen(scope.$index, scope.row)
-            }
-          }
-        }, [_vm._v("上传凭证")])], 1)], 1)], 1)]
+        }, [_vm._v("驳回付款")])], 1) : _vm._e()], 1)], 1)]
       }
     }])
   })], 1), _vm._v(" "), _c('div', {
