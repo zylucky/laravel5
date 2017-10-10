2c78775a21a8c34e530bf34f81f836db2acafaca
zhoumeng2012bj <zhoumeng2012bj@163.com>
2017-10-10 11:23:38 +0800

    mergin

--- resources/assets/js/components/Navigation.vue
+++ resources/assets/js/components/Navigation.vue
@@ -102,7 +102,6 @@
             },
             //退出登录
             logout: function () {
-
                 var _this = this;
                 this.$confirm('确认退出吗?', '提示', {
                     //type: 'warning'
