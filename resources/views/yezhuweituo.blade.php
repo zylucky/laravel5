5adcf0168030af2573e5730e7de270c9d19bf568
liyuequn <18511909124@163.com>
2017-11-27 11:28:09 +0800

    yezhuweit

--- resources/views/yezhuweituo.blade.php
+++ resources/views/yezhuweituo.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>业主委托</title>
     <style>
         body,html{
             height: 100%;
+            margin: 0px;
         }
         {{--.main{background: url({{asset('images/yezhuweituo.jpg')}}) no-repeat;height: 1000%;width: 200%;}--}}
     </style>
