6f575e30c4b3bee9f72d342638e4c5a76667bb2a
liyuequn <18511909124@163.com>
2017-11-27 10:52:49 +0800

    yezhuweituo

---
+++ resources/views/yezhuweituo.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body,html{
+            height: 100%;
+        }
+        {{--.main{background: url({{asset('images/yezhuweituo.jpg')}}) no-repeat;height: 1000%;width: 200%;}--}}
+    </style>
+</head>
+<body>
+    <div class="main">
+        <img src="{{asset('images/yezhuweituo.jpg')}}" width="100%" alt="">
+    </div>
+
+</body>
+</html>
