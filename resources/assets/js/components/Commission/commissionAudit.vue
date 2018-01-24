4dd72fbbbd8b716952a9eecf932f3aeac78e0a96
yuan <zylucying@163.com>
2018-01-24 17:06:55 +0800

    审批样式修改提交2018-01-24 17:06 pm

--- resources/assets/js/components/Commission/commissionAudit.vue
+++ resources/assets/js/components/Commission/commissionAudit.vue
@@ -96,13 +96,31 @@
             <tbody>
             <tr style=" text-align:left;background-color:#EEF1F6;height: 40px"><td colspan="6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 审批进度</td></tr>
             <tr style=" text-align:left; height: 40px"><td colspan="6">
-                <div style="margin-left:20px;margin-top:30px;width: 100%;height: 130px"  >
+                <!--<div style="margin-left:20px;margin-top:30px;width: 100%;height: 130px"  >-->
 
-                    <el-steps :active=shenpijindu align-center >
+                    <!--<el-steps :active=shenpijindu align-center >
                         <el-step v-for="(item, index) in options" :title="item.value" :key="index" :description="item.label"></el-step>
 
-                    </el-steps>
-                </div></td></tr>
+                    </el-steps>-->
+                <!--</div>-->
+                <ul class="plan_box">
+		        	<li v-for="(item, index) in options">
+		        		<p :class="item.shenpi==1 && !item.isfock?'zt':(item.shenpi!=1 && item.shenpi!=2 && item.isfock?'zt spz':(item.shenpi==2?'zt ybh':'zt dsp'))">
+		        			<span :class="item.shenpi==1 && !item.isfock?'xh':(item.shenpi!=1 && item.shenpi!=2 && item.isfock?'xh xh3':(item.shenpi==2?'xh xh2':'xh xh1'))">{{index + 1}}</span>
+		        			<span>
+								<i v-if='item.shenpi==1 && !item.isfock'>{{item.shenpi==1 && !item.isfock?"已通过":"待审批"}}</i>
+								<i v-if='item.shenpi==null && !item.isfock'>{{item.shenpi==null && !item.isfock?"待审批":''}}</i>
+								<i v-if='item.shenpi!=1 && item.shenpi!=2 && item.isfock'>审批中</i>
+								<i v-if='item.shenpi==2'>{{item.shenpi==2?"已驳回":"已审批"}}</i>
+							</span>
+		        			<span :class="item.shenpi==1 && !item.isfock?'tiao':(item.shenpi!=1 && item.shenpi!=2 && item.isfock?'tiao jb1':(item.shenpi==2?'tiao jb2':(item.shenpi==null && !item.isfock?'tiao jb3':'tiao jb')))"></span>
+		        		</p>
+		        		<p class="name">{{item.value}}</p>
+		        		<p class="date">{{item.label}}</p>
+		        		
+		        	</li>
+		        </ul>
+            </td></tr>
             </tbody>
         </table>
 
@@ -129,7 +147,9 @@
     .td2{
         width:240px;
     }
-
+	.el-upload--picture-card{
+		display: none !important;
+	}
 </style>
 <script>
 
@@ -159,7 +179,13 @@
                     console.log(value)
                     var newDate = new Date();
                     newDate.setTime(value);
-                    return newDate.toLocaleString();
+                    var hour = newDate.getHours();
+				    var minute = newDate.getMinutes();
+				    var second = newDate.getSeconds();
+				    if(hour<10){hour = '0' + hour;}
+				    if(minute<10){minute = '0' + minute;}
+				    if(second<10){second = '0' + second;}
+                    return newDate.toLocaleDateString() + ' ' + hour + ':' + minute + ':' + second;
                 }
             },
             handlePictureCardPreview(file) {
@@ -184,6 +210,8 @@
                         this.options.push({
                             value: res.data.shenPi[item].person,
                             label: this.changeDate(res.data.shenPi[item].shenpitime ),
+                            shenpi: res.data.shenPi[item].shenpi,
+                            isfock: res.data.shenPi[item].isfock,
                         });
                     }
                 });
@@ -261,3 +289,109 @@
         }
     }
 </script>
+<style scoped>
+	ul,li,dl,dd{
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+	.plan_box{
+		display: flex;
+		min-height: 50px;
+		height: auto;
+		font-size: 16px;
+		margin: 30px;
+	}
+	.plan_box p{
+		margin: 0;
+		padding: 0;
+		text-align: center;
+	}
+	.plan_box li{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 120px;
+		margin-right: 35px;
+	}
+	.zt{
+		position: relative;
+		width: 120px;
+		height: 34px;
+		line-height: 34px;
+		background: #1fa0fc;
+		color: #fff;
+		font-size: 16px;
+		text-align: center;
+		border-radius: 34px;
+		margin-bottom: 10px !important;
+	}
+	.zt .xh{
+		display: inline-block;
+		width: 20px;
+		height: 20px;
+		line-height: 20px;
+		border-radius: 50%;
+		background: #fff;
+		color: #1fa0fc;
+		text-align: center;
+	}
+	.zt i{
+		font-style: normal;
+	}
+	.name,.date{
+		color: #646464;
+	}
+	.tiao{
+		position: absolute;
+		left: -37.5px;
+		top: 50%;
+		margin-top: -1px;
+		display: inline-block;
+		width: 40px;
+		height: 2px;
+		background: #1fa0fc;
+	}
+	.plan_box li:first-child .tiao{
+		display: none;
+	}
+	.dsp{
+		/*待审批样式*/
+		background: #bbbbbb;
+	}
+	.xh1{
+		/*待审批文字颜色*/
+		color: #bbbbbb!important;
+	}
+	.ybh{
+		/*驳回样式*/
+		background: #ff7271;
+	}
+	.xh2{
+		/*驳回文字颜色*/
+		color: #ff7271!important;
+	}
+	.spz{
+		/*审批中*/
+		background: #fea843;
+	}
+	.xh3{
+		/*审批中文字颜色*/
+		color: #fea843!important;
+	}
+	.jb{
+		/*条渐变审批中到待审批*/
+		background: -webkit-linear-gradient(left,#fea843,#bbbbbb);
+	}
+	.jb1{
+		/*条渐变已通过到审批中*/
+		background: -webkit-linear-gradient(left,#20a1ff,#fea843);
+	}
+	.jb2{
+		/*条渐变已通过到已驳回*/
+		background: -webkit-linear-gradient(left,#20a1ff,#ff7271);
+	}
+	.jb3{
+		background: #bbbbbb;
+	}
+</style>
