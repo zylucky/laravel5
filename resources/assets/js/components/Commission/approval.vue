7be0a8e9b4dd90ea0fbc104642bb6dbd72d309b4
zhoumeng2012bj <zhoumeng2012bj@163.com>
2018-03-14 17:10:01 +0800

    佣金打印加月租金

--- resources/assets/js/components/Commission/approval.vue
+++ resources/assets/js/components/Commission/approval.vue
@@ -25,7 +25,7 @@
                 </tr>
                 <tr>
                     <th align="center" width="200" height="30"> 佣金金额:</th>
-                    <td align="center" width="400">¥{{approval.data.xsyongjin}}</td>
+                    <td align="center" width="400">¥{{toDecimal(approval.data.xsyongjin)}}</td>
                 </tr>
                 <tr>
                     <th align="center" width="200" height="30"> 佣金信息:</th>
@@ -33,7 +33,7 @@
                 </tr>
                 <tr>
                     <th align="center" width="200" height="30"> 月租金:</th>
-                    <td align="center" width="400">¥{{approval.data.yzj}}</td>
+                    <td align="center" width="400">¥{{toDecimal(approval.data.yzj)}}</td>
                 </tr>
                 <tr>
                     <th align="center" width="200" height="30"> 佣金计算公式:</th>
@@ -169,7 +169,26 @@
                if(num){
                    return num.replace(/[\s]/g, '').replace(/(\d{4})(?=\d)/g, "$1 ")
                }
-            }
+            },
+            toDecimal(x) {
+                var f = parseFloat(x);
+                if (isNaN(f)) {
+                    return false;
+                }
+                var f = Math.round(x * 100) / 100;
+                var s = f.toString();
+                var rs = s.indexOf('.');
+                if (rs < 0) {
+                    rs = s.length;
+                    s += '.';
+                }
+                while (s.length <= rs + 2) {
+                    s += '0';
+                }
+
+                return  s.split('').reverse().join('').replace(/(\d{3}(?=\d)(?!\d+\.|$))/g, '$1,').split('').reverse().join('');
+
+            },
         },
         mounted(){
             this.getInfo();
