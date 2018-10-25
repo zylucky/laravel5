afc0b1155d97caf9805f50ec3d64cf858d05a4e0
yuan <zyluckying@163.com>
2018-10-25 13:49:50 +0800

    zhang2

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('nav')
-    <router-view></router-view>
-@endsection
-
-
-
-
--- resources/views/test.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <passport-clients></passport-clients>
-    </div>
-
-@endsection
